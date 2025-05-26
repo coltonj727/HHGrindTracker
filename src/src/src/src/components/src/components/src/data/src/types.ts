0d7351be6576572df65ab38e37193d3ed6f0bb14
coltonj727 <Jorgensencolton612@gmail.com>
2025-05-26 15:53:42 -0500

    Create types.ts

---
+++ src/src/src/src/components/src/components/src/data/src/types.ts
@@ -0,0 +1,10 @@
+export interface Grind {
+  id: number;
+  species: string;
+  map: string;
+  kills: number;
+  diamonds: number;
+  rares: number;
+  goHarvested: boolean;
+  createdAt: string;
+}
